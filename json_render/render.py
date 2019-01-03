b54d0bcd9ce736a5dd40c57a74b60d3348b1e852
Brightcells <brightcells@gmail.com>
2019-01-03 17:25:57 +0800

    :sparkles: Support ignore_blank

--- json_render/render.py
+++ json_render/render.py
@@ -8,7 +8,7 @@ from django.shortcuts import render
 
 def json_render(request, template_name, dictionary=None, unjsondumpsdict=None,
                 setcookie=False, delcookie=False, signedcookie=True, cookie_key=None, cookie_value=None, cookie_salt=None,
-                cookie_max_age=None, cookie_expires=None, cookie_path='/', cookie_domain=None, cookie_secure=False, cookie_httponly=False):
+                cookie_max_age=None, cookie_expires=None, cookie_path='/', cookie_domain=None, cookie_secure=False, cookie_httponly=False, ignore_blank=False):
 
     unjsondumpsdict = unjsondumpsdict or {}
     unjsondumpsdict['data'] = json.dumps(dictionary or {}, cls=DjangoJSONEncoder)
@@ -16,6 +16,9 @@ def json_render(request, template_name, dictionary=None, unjsondumpsdict=None,
     response = render(request, template_name, unjsondumpsdict)
 
     if setcookie:
+        if ignore_blank and not cookie_value:
+            return response
+
         if signedcookie:
             response.set_signed_cookie(cookie_key, cookie_value, salt=cookie_salt, **{
                 'max_age': cookie_max_age,
